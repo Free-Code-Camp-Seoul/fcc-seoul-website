e5f44d7caa558b7bbf5c39d297a6c1e500ea7eb1
Ivan Saldano <ivanms1@gmail.com>
2022-11-20 19:43:30 +0900

    feat: members page

---
+++ pages/members/index.tsx
@@ -0,0 +1,3 @@
+import Members from "@/pages/Members";
+
+export default Members;
---
+++ src/pages/Members/Members.tsx
@@ -0,0 +1,59 @@
+import MemberCard from "@/components/MemberCard";
+import React from "react";
+
+import styles from "./Members.module.scss";
+
+interface MembersProps {}
+
+const Members = ({}: MembersProps) => {
+  return (
+    <div className={styles.Members}>
+      <p className={styles.Title}>Meet some of our members</p>
+      <div className={styles.Members}>
+        {MEMBERS.map((member) => (
+          <MemberCard key={member.github} member={member} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+const MEMBERS = [
+  {
+    name: "Andrei Calinescu",
+    github: "https://github.com/drecali",
+    bio: "I'm a software engineer and I love to build things.",
+    image: "https://avatars.githubusercontent.com/u/24983797?v=4",
+    interests: ["Web Development", "Machine Learning", "Data Science"],
+  },
+  {
+    name: "Toby",
+    github: "https://github.com/Tobio89",
+    bio: "I'm a software engineer and I love to build things.",
+    image: "https://avatars.githubusercontent.com/u/44494591?v=4",
+    interests: ["Web Development", "Machine Learning", "Data Science"],
+  },
+  {
+    name: "Simon Lee",
+    github: "https://github.com/simonhlee97",
+    bio: "I'm a software engineer and I love to build things.",
+    image: "https://avatars.githubusercontent.com/u/22103041?v=4",
+    interests: ["Web Development", "Machine Learning", "Data Science"],
+  },
+  {
+    name: "Joe Mcgee",
+    github: "https://github.com/joemcgee4151986",
+    bio: "I'm a software engineer and I love to build things.",
+    image: "https://avatars.githubusercontent.com/u/16703432?v=4",
+    interests: ["Web Development", "Machine Learning", "Data Science"],
+  },
+  {
+    name: "Ivan Saldano",
+    github: "https://github.com/ivanms1",
+    bio: "I'm a software engineer and I love to build things.",
+    image: "https://avatars.githubusercontent.com/u/29688963?v=4",
+    interests: ["Web Development", "Machine Learning", "Data Science"],
+  },
+];
+
+export default Members;
