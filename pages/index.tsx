48ca26e090020d9b5c5ee330b09f2310010aabf5
Ivan Saldano <ivanms1@gmail.com>
2023-02-25 19:45:14 +0900

    feat: i18n

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,19 @@
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { GetStaticProps } from "next";
+
 import Home from "src/pages/Home";
 
+import { DEFAULT_LOCALE } from "@/const";
+
 export default Home;
+
+export const getStaticProps: GetStaticProps = async ({ locale }) => {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale || DEFAULT_LOCALE, [
+        "home",
+        "common",
+      ])),
+    },
+  };
+};
