66c776cabd4158c1108d92f09566047667d30178
Ivan <ivan@lunit.io>
2024-05-17 18:09:59 +0900

    chore: add image to seo

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -38,7 +38,6 @@ function Home({ eventList }: HomeProps) {
           {t("more-info")}
         </a>
       </div>
-
       <>{View}</>
       <NextSeo
         title="FreeCodeCamp Seoul 🇰🇷"
@@ -50,6 +49,14 @@ function Home({ eventList }: HomeProps) {
           title: "FreeCodeCamp Seoul 🇰🇷",
           description:
             "FreeCodeCamp Seoul is a community of developers in Seoul, South Korea. We meet regularly to learn and share our knowledge. Join us!",
+          images: [
+            {
+              url: "/assets/images/fcc-logo.webp",
+              width: 1000,
+              height: 420,
+              alt: "FreeCodeCamp Seoul",
+            },
+          ],
         }}
         twitter={{
           handle: "@handle",
