115f1ca5797cc10d83aea0eb0e385f6fc918c751
Andrei Calinescu <andreicali@gmail.com>
2024-10-26 18:58:00 +0900

    #97 - Fix invalid date format on home page (#98)
    * chore: use RSS link to `getMeetupInfo`
    
    * fix: get event times from RSS feed instead of HTML text
    
    * chore: improve regex to catch both AM and PM times

--- src/services/meetup/index.ts
+++ src/services/meetup/index.ts
@@ -1,17 +1,20 @@
-import { MEETUP_LINK } from "@/const";
+import { MEETUP_RSS_LINK } from "@/const";
 
 export const getMeetupInfo = async () => {
-  const meetupResponse = await fetch(MEETUP_LINK, { cache: "no-store" });
+  const meetupResponse = await fetch(MEETUP_RSS_LINK, { cache: "no-store" });
   const meetupText = await meetupResponse.text();
-  // create an array with all the time elements with the class "text-[#00829B] text-sm font-medium uppercase"
-  const eventList = meetupText.match(
-    /<time class="text-\[#00829B\] text-sm font-medium uppercase">(.*?)<\/time>/g
-  );
+  // create an array with all the meetup times from the RSS feed"
+  const eventList = meetupText.match(/<p>Sunday.+M<\/p>/g);
 
   // remove the html tags and timezone from the array
   const trimmedEventList = eventList?.map((event) => {
     return event.replace(/(<([^>]+)>)/gi, "").replace(/(KST)/gi, "");
   });
 
-  return trimmedEventList;
+  const correctFormatEventList = trimmedEventList?.map((badFormatEventTime) =>
+    // replace "at" with "2024" to make the date valid JS date format
+    badFormatEventTime.replace("at", "2024")
+  );
+
+  return correctFormatEventList;
 };
